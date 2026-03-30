54be781f06bb48ae97a2486ff8133ebc4505153e
zsliu98 <zsliu98@gmail.com>
2026-03-30 19:59:21 -0400

    fix: still should use unroll version

--- zlfft_impl/hybrid_aosoa1.hpp
+++ zlfft_impl/hybrid_aosoa1.hpp
@@ -447,20 +447,53 @@ namespace zlfft {
                 common::radix4_last_pass_fused_aosoa(current_in, out_aos, M, width, uw_ptr);
             }
 
-            static constexpr size_t TILE_K = 16;
-            static constexpr size_t TILE_C = 64;
+            static constexpr size_t TILE = 4;
 
-            for (size_t k_block = 0; k_block < M; k_block += TILE_K) {
-                const size_t k_end = std::min(M, k_block + TILE_K);
-                for (size_t c_block = 0; c_block < l_; c_block += TILE_C) {
-                    const size_t c_end = std::min(l_, c_block + TILE_C);
-                    for (size_t k = k_block; k < k_end; ++k) {
-                        std::complex<F>* __restrict out_row = out_buffer.data() + k * l_;
-#pragma clang loop vectorize(enable) interleave(enable)
-                        for (size_t c = c_block; c < c_end; ++c) {
-                            out_row[c] = aos_matrix[c * M_padded + k];
-                        }
-                    }
+            for (size_t k_block = 0; k_block < M; k_block += TILE) {
+                for (size_t c_block = 0; c_block < l_; c_block += TILE) {
+                    const std::complex<F>* src0 = aos_matrix + (c_block + 0) * M_padded + k_block;
+                    const std::complex<F>* src1 = aos_matrix + (c_block + 1) * M_padded + k_block;
+                    const std::complex<F>* src2 = aos_matrix + (c_block + 2) * M_padded + k_block;
+                    const std::complex<F>* src3 = aos_matrix + (c_block + 3) * M_padded + k_block;
+
+                    std::complex<F>* dst0 = out_buffer.data() + (k_block + 0) * l_ + c_block;
+                    std::complex<F>* dst1 = out_buffer.data() + (k_block + 1) * l_ + c_block;
+                    std::complex<F>* dst2 = out_buffer.data() + (k_block + 2) * l_ + c_block;
+                    std::complex<F>* dst3 = out_buffer.data() + (k_block + 3) * l_ + c_block;
+
+                    auto r00 = src0[0];
+                    auto r01 = src0[1];
+                    auto r02 = src0[2];
+                    auto r03 = src0[3];
+                    auto r10 = src1[0];
+                    auto r11 = src1[1];
+                    auto r12 = src1[2];
+                    auto r13 = src1[3];
+                    auto r20 = src2[0];
+                    auto r21 = src2[1];
+                    auto r22 = src2[2];
+                    auto r23 = src2[3];
+                    auto r30 = src3[0];
+                    auto r31 = src3[1];
+                    auto r32 = src3[2];
+                    auto r33 = src3[3];
+
+                    dst0[0] = r00;
+                    dst0[1] = r10;
+                    dst0[2] = r20;
+                    dst0[3] = r30;
+                    dst1[0] = r01;
+                    dst1[1] = r11;
+                    dst1[2] = r21;
+                    dst1[3] = r31;
+                    dst2[0] = r02;
+                    dst2[1] = r12;
+                    dst2[2] = r22;
+                    dst2[3] = r32;
+                    dst3[0] = r03;
+                    dst3[1] = r13;
+                    dst3[2] = r23;
+                    dst3[3] = r33;
                 }
             }
         }
